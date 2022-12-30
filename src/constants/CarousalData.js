6aa7431cb34a7a9273712a344d50eb60a0f30626
Gloviza <shubhashish.vim@gmail.com>
2022-12-31 02:58:10 +0530

    transformed into pwa

--- src/constants/CarousalData.js
+++ src/constants/CarousalData.js
@@ -1,49 +1,49 @@
 export const homeCarousalData = [
-  { imgPath: "nb_img1.jpg", alternateName: "abc", id: "1" },
-  { imgPath: "nb_img2.jpg", alternateName: "abc", id: "2" },
+  { imgPath: "nb_img1.jpg", alternateName: "NutrineoBiotech", id: "1" },
+  { imgPath: "nb_img2.jpg", alternateName: "NutrineoBiotech", id: "2" },
 ];
 
 export const pediatricCarousalData = [
-  { imgPath: "nb_img3.jpg", alternateName: "abc", id: "3" },
-  { imgPath: "nb_img4.jpg", alternateName: "abc", id: "4" },
-  { imgPath: "nb_img5.jpg", alternateName: "abc", id: "5" },
-  { imgPath: "nb_img6.jpg", alternateName: "abc", id: "6" },
+  { imgPath: "nb_img3.jpg", alternateName: "NutrineoBiotech", id: "3" },
+  { imgPath: "nb_img4.jpg", alternateName: "NutrineoBiotech", id: "4" },
+  { imgPath: "nb_img5.jpg", alternateName: "NutrineoBiotech", id: "5" },
+  { imgPath: "nb_img6.jpg", alternateName: "NutrineoBiotech", id: "6" },
 ];
 
 export const gynP1CarousalData = [
-  { imgPath: "nb_img7.jpg", alternateName: "abc", id: "7" },
-  { imgPath: "nb_img8.jpg", alternateName: "abc", id: "8" },
-  { imgPath: "nb_img9.jpg", alternateName: "abc", id: "9" },
-  { imgPath: "nb_img10.jpg", alternateName: "abc", id: "10" },
+  { imgPath: "nb_img7.jpg", alternateName: "NutrineoBiotech", id: "7" },
+  { imgPath: "nb_img8.jpg", alternateName: "NutrineoBiotech", id: "8" },
+  { imgPath: "nb_img9.jpg", alternateName: "NutrineoBiotech", id: "9" },
+  { imgPath: "nb_img10.jpg", alternateName: "NutrineoBiotech", id: "10" },
 ];
 
 export const gynP2CarousalData = [
-  { imgPath: "nb_img18.jpg", alternateName: "abc", id: "18" },
-  { imgPath: "nb_img19.jpg", alternateName: "abc", id: "19" },
-  { imgPath: "nb_img20.jpg", alternateName: "abc", id: "20" },
+  { imgPath: "nb_img18.jpg", alternateName: "NutrineoBiotech", id: "18" },
+  { imgPath: "nb_img19.jpg", alternateName: "NutrineoBiotech", id: "19" },
+  { imgPath: "nb_img20.jpg", alternateName: "NutrineoBiotech", id: "20" },
 ];
 
 export const phyP1CarousalData = [
-  { imgPath: "nb_img11.jpg", alternateName: "abc", id: "11" },
-  { imgPath: "nb_img12.jpg", alternateName: "abc", id: "12" },
-  { imgPath: "nb_img13.jpg", alternateName: "abc", id: "13" },
+  { imgPath: "nb_img11.jpg", alternateName: "NutrineoBiotech", id: "11" },
+  { imgPath: "nb_img12.jpg", alternateName: "NutrineoBiotech", id: "12" },
+  { imgPath: "nb_img13.jpg", alternateName: "NutrineoBiotech", id: "13" },
 ];
 
 export const phyP2CarousalData = [
-  { imgPath: "nb_img21.jpg", alternateName: "abc", id: "21" },
-  { imgPath: "nb_img22.jpg", alternateName: "abc", id: "22" },
-  { imgPath: "nb_img23.jpg", alternateName: "abc", id: "23" },
+  { imgPath: "nb_img21.jpg", alternateName: "NutrineoBiotech", id: "21" },
+  { imgPath: "nb_img22.jpg", alternateName: "NutrineoBiotech", id: "22" },
+  { imgPath: "nb_img23.jpg", alternateName: "NutrineoBiotech", id: "23" },
 ];
 
 export const surP1CarousalData = [
-  { imgPath: "nb_img14.jpg", alternateName: "abc", id: "14" },
-  { imgPath: "nb_img15.jpg", alternateName: "abc", id: "15" },
-  { imgPath: "nb_img16.jpg", alternateName: "abc", id: "16" },
-  { imgPath: "nb_img17.jpg", alternateName: "abc", id: "17" },
+  { imgPath: "nb_img14.jpg", alternateName: "NutrineoBiotech", id: "14" },
+  { imgPath: "nb_img15.jpg", alternateName: "NutrineoBiotech", id: "15" },
+  { imgPath: "nb_img16.jpg", alternateName: "NutrineoBiotech", id: "16" },
+  { imgPath: "nb_img17.jpg", alternateName: "NutrineoBiotech", id: "17" },
 ];
 
 export const surP2CarousalData = [
-  { imgPath: "nb_img21.jpg", alternateName: "abc", id: "24" },
-  { imgPath: "nb_img22.jpg", alternateName: "abc", id: "25" },
-  { imgPath: "nb_img23.jpg", alternateName: "abc", id: "26" },
+  { imgPath: "nb_img21.jpg", alternateName: "NutrineoBiotech", id: "24" },
+  { imgPath: "nb_img22.jpg", alternateName: "NutrineoBiotech", id: "25" },
+  { imgPath: "nb_img23.jpg", alternateName: "NutrineoBiotech", id: "26" },
 ];
