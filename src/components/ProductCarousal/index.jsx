b7b5ac845e09ab8dd6bfd34582b5311311441aae
Gloviza <shubhashish.vim@gmail.com>
2023-01-01 15:21:58 +0530

    making carousal controlled

--- src/components/ProductCarousal/index.jsx
+++ src/components/ProductCarousal/index.jsx
@@ -1,19 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import "./styles.css";
 
 const ProductCarousal = ({ carousalData }) => {
+  const [currIndex, setCurrIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setCurrIndex(selectedIndex);
+  };
+
+  useEffect(() => {
+    setCurrIndex(0);
+  }, [carousalData]);
+
   return (
     <>
       <div className="background">
         <div>
           <Carousel
+            activeIndex={currIndex}
             prevLabel={""}
             nextLabel={""}
             interval={null}
             wrap={false}
             touch={false}
+            onSelect={handleSelect}
           >
             {carousalData.map(({ imgPath, alternateName, id }) => {
               return (
