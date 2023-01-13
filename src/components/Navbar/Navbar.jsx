f7a29c8b57dcc5bc51b6d552a052ab76b81df4e9
Gloviza <shubhashish.vim@gmail.com>
2023-01-14 01:33:06 +0530

    navbar close on item-select

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { slide as Menu } from "react-burger-menu";
 // import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -7,31 +7,64 @@ const Navbar = () => {
   // showSettings (event) {
   //   event.preventDefault();
   // }
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <Menu width={"auto"}>
-      <Link className="menu-item" to="/">
+    <Menu
+      width={"auto"}
+      isOpen={menuOpen}
+      onStateChange={(state) => setMenuOpen(state.isOpen)}
+    >
+      <Link className="menu-item" onClick={() => setMenuOpen(false)} to="/">
         Home
       </Link>
-      <Link className="menu-item" to="/Pediatric">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Pediatric"
+      >
         Pediatric
       </Link>
-      <Link className="menu-item" to="/Gyn-P1">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Gyn-P1"
+      >
         Gyn-P1
       </Link>
-      <Link className="menu-item" to="/Gyn-P2">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Gyn-P2"
+      >
         Gyn-P2
       </Link>
-      <Link className="menu-item" to="/Phy-P1">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Phy-P1"
+      >
         Phy-P1
       </Link>
-      <Link className="menu-item" to="/Phy-P2">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Phy-P2"
+      >
         Phy-P2
       </Link>
-      <Link className="menu-item" to="/Sur-P1">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Sur-P1"
+      >
         Sur-P1
       </Link>
-      <Link className="menu-item" to="/Sur-P2">
+      <Link
+        className="menu-item"
+        onClick={() => setMenuOpen(false)}
+        to="/Sur-P2"
+      >
         Sur-P2
       </Link>
 
